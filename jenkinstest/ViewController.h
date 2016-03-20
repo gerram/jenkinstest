9b625b83e357e7574e1af66506e54791d266823e
George Malushkin <george@miniGiant.local>
2016-03-20 11:13:55 +0300

    Start Repo

---
+++ jenkinstest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkinstest
+//
+//  Created by George Malushkin on 20/03/16.
+//  Copyright © 2016 George Malushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jenkinstest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jenkinstest
+//
+//  Created by George Malushkin on 20/03/16.
+//  Copyright © 2016 George Malushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
